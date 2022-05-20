44fcdc1c5890ebba630a663c89719f4ecb6d1b0f
a@b.com <d@d.com>
2022-05-21 01:08:19 +0500

    Book me category home page changes and inner styling

--- wp-content/themes/prologe-lite/custom_home.php
+++ wp-content/themes/prologe-lite/custom_home.php
@@ -2,7 +2,9 @@
 
 <?php get_header(); ?>
 
-<section class="slider-area">
+<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+
+<section class="slider-area" id="particles-js">
   <div class="container">
 
     <div class="caption">
@@ -10,12 +12,13 @@
       <h4>Robert Frost</h4>
     </div>
 
-  	<video width="320" height="240" controls>
+  	<!-- <video width="320" height="240" controls>
 		  <source src="http://localhost/projects/AM-Moiz/jewelrysite/wp-content/uploads/2022/05/istockphoto-460814742-640_adpp_is.mp4" type="video/mp4">
 		  Your browser does not support the video tag.
-	  </video>
+	  </video> -->
   </div>
 </section>
  
+
 
 <?php get_footer(); ?>
